d041c02588bc2d0e1465297eb6aa0fd468d24a82
bhrbghr <ErikaOjaghi@gmail.com>
2025-03-02 19:37:32 +0330

    changed the font

--- survey-form/src/Main.java
+++ survey-form/src/Main.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import packages.informationPage;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
         // Create and configure the main frame
-        JFrame frame = new JFrame("فرم نظرسنجی");
+        JFrame frame = new JFrame("Survey Form");
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,12 +18,16 @@ public class Main {
         panel.setLayout(null);
         panel.setBounds(0, 0, 600, 600);
 
+        //font
+        Font font = new Font("Arial",Font.BOLD, 16);
+
         // Create title label
-        JLabel title = new JLabel("فرم نظرسنجی");
+        JLabel title = new JLabel("Survey Form");
+        title.setFont(font);
         title.setBounds(10, 10, 100, 20);
 
         // Create start button
-        JButton startButton = new JButton("شروع!");
+        JButton startButton = new JButton("Start!");
         startButton.setBounds(10, 40, 100, 20);
 
         // Add components to the panel
@@ -46,6 +52,8 @@ public class Main {
             frame.revalidate();
             frame.repaint();
         });
+
+        //
 
         // Make the frame visible
         frame.setVisible(true);
